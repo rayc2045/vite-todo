57b98df747a9930c473c3fc9adac0f469bbbe92d
Ray C <rayc2045@gmail.com>
2021-06-11 15:53:45 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,9 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}'
+    // './index.html',
+    // './src/**/*.{vue,js,ts,jsx,tsx}'
+    './src/**/*.{html,js}'
   ],
   presets: [],
   darkMode: false, // or 'media' or 'class'
